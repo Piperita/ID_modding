8892e87ca5c7eb2886b2d74b81bb18f0ad65bef0
Piperita <Piperita@users.noreply.github.com>
2015-08-02 16:30:16 +0200

    Update and rename .gitignore to .js

---
+++ .js
@@ -0,0 +1,71 @@
+//functionality hide-button
+function clickHideButton(instance) {
+ instance.nextSibling.style.display = 'none'
+}
+
+//functionality show-button
+function clickShowButton(instance) {
+  instance.nextSibling.nextSibling.height = (parseInt($('.popClass').outerWidth()*0.5625))+'px';
+  //Youtube
+  if (/.*[Yy]outube.*/.test(instance.previousSibling.href) == true && /.*m\.[Yy]outube.*/.test(instance.previousSibling.href) == false) {
+    ausl    = instance.previousSibling.href.replace("watch?v=", "embed/");
+    instance.nextSibling.nextSibling.src = ausl;
+    instance.nextSibling.nextSibling.style.display = 'block'
+  //youtu.be
+  } else if (/.*[Yy]outu.be.*/.test(instance.previousSibling.href) == true){
+      ausl    = instance.previousSibling.href.replace("youtu.be", "www.youtube.com/embed/");
+      instance.nextSibling.nextSibling.src = ausl;
+      instance.nextSibling.nextSibling.style.display = 'block'   
+  //m.youtube
+  } else if (/.*m\.[Yy]outube.*/.test(instance.previousSibling.href) == true){
+      ausl    = instance.previousSibling.href.replace("watch?v=", "embed/").replace("m.you","www.you");
+      instance.nextSibling.nextSibling.src = ausl;
+      instance.nextSibling.nextSibling.style.display = 'block'    
+  //vine
+  } else if (/.*vine\.co.*/.test(instance.previousSibling.href) == true){
+      instance.nextSibling.nextSibling.src = instance.previousSibling.href+'/embed/simple';
+      instance.nextSibling.nextSibling.style.display = 'block'
+  //twitter
+  } else if (/https?\:\/\/t\.co.*/.test(instance.previousSibling.href) == true || /.*twitter\.com*/.test(instance.previousSibling.href) == true){
+      ausl     = instance.previousSibling.href.replace(":", "%3A").replace("/","%2F");
+      instance.nextSibling.nextSibling.src = "http://twitframe.com/show?url="+ausl
+      instance.nextSibling.nextSibling.style.display = 'block'
+  //ToggleEQD mobile
+  } else if (/.*equestriadaily\.com.*/.test(instance.previousSibling.href) == true){
+      //If mobile return mobile
+      if (/.*\?m=1.*/.test(instance.previousSibling.href) == true){ 
+        instance.nextSibling.nextSibling.src = instance.previousSibling.href
+        instance.nextSibling.nextSibling.style.display = 'block'
+      //If explicit dektop turn mobile
+      } else if (/.*\?m=0.*/.test(instance.previousSibling.href) == true){ 
+        ausl     = instance.previousSibling.href.replace("?m=0", "?m=1");
+        instance.nextSibling.nextSibling.src = ausl  
+        instance.nextSibling.nextSibling.style.display = 'block'
+      //If main site return mobile ---> no height adjustment?????
+      } else if (/.*equestriadaily\.com\/?$/.test(instance.previousSibling.href) == true) { 
+        ausl     = instance.previousSibling.href.replace(".com/", ".com").replace(".com", ".com?m=1");
+        instance.nextSibling.nextSibling.src = ausl
+        instance.nextSibling.nextSibling.style.display = 'block'
+      //If post turn mobile
+      } else { 
+        ausl     = instance.previousSibling.href.replace(".html", ".html?m=1");
+        instance.nextSibling.nextSibling.src = ausl
+        instance.nextSibling.nextSibling.style.display = 'block'
+      }
+  // Return null when livestream.com (redirect) or twitch.tv (system for embed unclear)
+  } else if (/.*livestream\.com.*/.test(instance.previousSibling.href) == true || /.*twitch\.tv*/.test(instance.previousSibling.href) == true){
+    //add notice later
+  //everything else
+  } else {
+      instance.nextSibling.nextSibling.src = instance.previousSibling.href
+      instance.nextSibling.nextSibling.style.display = 'block'  
+  }
+}
+
+//Adds elements
+function popupLinks() {
+  $('.idc-c-t-inner').find('a').after("<iframe id='frameBoard' class='popClass' src='about:blank' width = '100%'></iframe>");
+  $('.idc-c-t-inner').find('a').after("<button type='button' onclick='clickHideButton(this)'>-</button>")
+  $('.idc-c-t-inner').find('a').after("<button type='button' onclick='clickShowButton(this)'>+</button>")
+  $('.popClass').css({display: 'none'})
+}
