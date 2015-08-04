36f8cf94377d38bd82c9712ce87239ed9aa824b3
Piperita <Piperita@users.noreply.github.com>
2015-08-04 19:17:29 +0200

    Update and rename count.js to counter_loop.js

--- CountReplies/counter_loop.js
+++ CountReplies/counter_loop.js
@@ -1,28 +1,35 @@
 var formf =   [];
+var count =   0
+var v= 0
+if ($('#idc-pager').length>0){
+  var v=$('#idc-pager').children(); 
+  v=v[parseInt(v.length-3)].innerHTML-1;
+}
 
 function getPoster(){
   c=$('.idc-collapselink_closed')
   var author = [];
   var replies= [];
   var form =   [];
+  var text = [];
   for (i = 1; i < c.length; i++) { 
     if (!c[i].parentElement.parentElement.parentElement.children[0].classList.contains ('idc-twitter') && !c[i].parentElement.parentElement.parentElement.children[0].classList.contains ('idc-anonymous')) {
       author[i] = c[i].parentElement.parentElement.parentElement.children[0].children[0].children[0].children[3].children[0].innerHTML;
+      text[i] = c[i].parentElement.parentElement.children[1].children[0].innerHTML
       if (parseInt(c[i].innerHTML)){
         replies[i] = parseInt(c[i].innerHTML)
       } else {
         replies[i] = 0;
       } 
-      form[i] =  replies[i]+'\t'+author[i]
+      form[i] =  replies[i]+'\t'+author[i]+'\t'+text[i]
     }
   } 
   formf = formf.concat(form);
-
-  if ($('#idc-pager').length > 0){
-    a=$('.idc-sel')
-    a=a[a.length-1]
-    if (a.nextSibling.className != 'idc-clear') {
-      a.nextSibling.click();
+ 	console.log(count)
+  count++
+  if (v > 0){
+    if (count <= v) {
+      IDPageLoad(count);
     } else {
         formf[0] = "replies\tauthor"
         formf = formf.filter(Boolean)
@@ -38,13 +45,14 @@ function getPoster(){
         }
         $('#idc-commentcount').append(csvElement);
     }
-    if (a.nextSibling.className != 'idc-clear') {
+
+    if (count <= v) {
       myVar = setTimeout(function(){ 
         getPoster()
       }, 3000);
     }
   } else {
-        formf[0] = "replies\tauthor"
+        formf[0] = "replies\tauthor\tcomment"
         formf = formf.filter(Boolean)
         formf = formf.join("\n")
         var csvElement = document.createElement('a');
@@ -60,4 +68,4 @@ function getPoster(){
   }
 }
 
-getPoster();
+getPoster()
