bcbc693cd4b01e7c72d4694729561c7c1291ed93
Piperita <Piperita@users.noreply.github.com>
2015-08-07 10:30:56 +0200

    Legacy-Support
    Both versions are now identical, this path will be deleted in some time.

--- CountReplies/alternativeLoop/counter_loop.js
+++ CountReplies/alternativeLoop/counter_loop.js
@@ -1,10 +1,6 @@
+var currentPage = 0;
+var totalCommentThreads = 0;
 var formf =   [];
-var count =   0
-var v= 0
-if ($('#idc-pager').length>0){
-  var v=$('#idc-pager').children(); 
-  v=v[parseInt(v.length-3)].innerHTML-1;
-}
 
 function getPoster(){
   c=$('.idc-collapselink_closed')
@@ -25,33 +21,22 @@ function getPoster(){
     }
   } 
   formf = formf.concat(form);
- 	console.log(count)
-  count++
-  if (v > 0){
-    if (count <= v) {
-      IDPageLoad(count);
-    } else {
-        formf[0] = "replies\tauthor"
-        formf = formf.filter(Boolean)
-        formf = formf.join("\n")
-        var csvElement = document.createElement('a');
-        csvElement.id = 'csv_download'
-        csvElement.innerHTML = " (Download CSV)";
-        csvElement.href     = 'data:application/csv;charset=utf-8,' + encodeURIComponent(formf);
-        csvElement.target   = '_blank';
-        csvElement.download = 'myFile.csv';
-        if ($('#csv_download').length >0){
-          $('#csv_download').remove()
-        }
-        $('#idc-commentcount').append(csvElement);
-    }
+    console.log("Page " + (currentPage+1));
+}
 
-    if (count <= v) {
-      myVar = setTimeout(function(){ 
-        getPoster()
-      }, 3000);
-    }
-  } else {
+//IntenseDebate action that fires after a new comment page has been loaded.
+function getReplies(){
+  id_add_action('page_load', function(pageObj) {
+    getPoster();
+    
+    //If there is another page, load it.
+    if($('#idc-pager .idc-sel').next('a').length) {
+        currentPage++;
+        IDPageLoad(currentPage);
+    } else {
+        //Remove the action we added.
+        var lastAction = id_action_list['page_load'].length-1;
+        id_action_list['page_load'][lastAction] = undefined
         formf[0] = "replies\tauthor\tcomment"
         formf = formf.filter(Boolean)
         formf = formf.join("\n")
@@ -65,7 +50,15 @@ function getPoster(){
           $('#csv_download').remove()
         }
         $('#idc-commentcount').append(csvElement);
-  }
+    }
+  });
+
+   //In case we are not on the first page already.
+   IDPageLoad(currentPage);
 }
 
-getPoster()
+var countLink = document.createElement('a');
+countLink.id = 'countLink'
+countLink.innerHTML = "For Bluefury";
+countLink.href     = 'javascript: getReplies()'
+$('#idc-sortLinks').children().append("|").append(countLink)
