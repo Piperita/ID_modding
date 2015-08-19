995358c1842ab90f24fdb10a5457c8d57fc513c3
Piperita <Piperita@users.noreply.github.com>
2015-08-19 19:56:22 +0200

    added 'carrier' to list

--- Filter/filter.js
+++ Filter/filter.js
@@ -1,6 +1,7 @@
 function circFilt(content) {
   content = content.replace('cialis','cia<b></b>lis');
-  content = content.replace('patreon','pat<b></b>reon'); 
+  content = content.replace('patreon','pat<b></b>reon');
+  content = content.replace('carrier','car<b></b>rier');
   content = content.replace('viagra','via<b></b>gra'); 
   content = content.replace('pharmacy','phar<b></b>macy');
   return content
