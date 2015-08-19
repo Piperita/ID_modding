be44b3a78f58aa958b813acdff38afe80766a477
Piperita <Piperita@users.noreply.github.com>
2015-08-19 13:33:13 +0200

    Added 'patreon' to list

--- Filter/filter.js
+++ Filter/filter.js
@@ -1,7 +1,8 @@
 function circFilt(content) {
-  content = content.replace('pharmacy','phar<b></b>macy'); 
-  content = content.replace('viagra','via<b></b>gra'); 
   content = content.replace('cialis','cia<b></b>lis');
+  content = content.replace('patreon','pat<b></b>reon'); 
+  content = content.replace('viagra','via<b></b>gra'); 
+  content = content.replace('pharmacy','phar<b></b>macy');
   return content
 }
 id_add_filter('pre_comment_text',circFilt);
