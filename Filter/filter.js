55dd4da81ef88cb4f420e4a11724d42c36048329
Piperita <Piperita@users.noreply.github.com>
2015-08-27 14:08:51 +0200

    Update filter.js

--- Filter/filter.js
+++ Filter/filter.js
@@ -2,7 +2,8 @@ function circFilt(content) {
   content = content.replace('cialis','cia<b></b>lis');
   content = content.replace('patreon','pat<b></b>reon');
   content = content.replace('carrier','car<b></b>rier');
-  content = content.replace('viagra','via<b></b>gra'); 
+  content = content.replace('viagra','via<b></b>gra');
+  content = content.replace('penis','pen<b></b>is');
   content = content.replace('cock','co<b></b>ck'); 
   content = content.replace('pharmacy','phar<b></b>macy');
   return content
