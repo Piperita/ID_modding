ae1b48a6232ff779ac82fd17203feb975957eae7
Piperita <Piperita@users.noreply.github.com>
2015-08-30 00:45:04 +0200

    Added titles to buttons; css-formatting

--- Editor/editor.js
+++ Editor/editor.js
@@ -73,8 +73,8 @@ var zyx = $("#txtComment")
 //-----------------------
 //Creates Button-Area----
 //-----------------------
-var cont1 =   '<div id="ButtonContainerNew"><button type="button" onclick="test(xyz)" style="">Preview</button><button type="button" onclick="strikeWord(xyz)"><strike>S</strike></button><button type="button" onclick="boldWord(xyz)"><b>B</b></button><button type="button" onclick="underlineWord(xyz)"><u>U</u></button><button type="button" onclick="italizeWord(xyz)"><i>I</i></button><button type="button" onclick="unorderWord(xyz)">*</button><button type="button" onclick="orderWord(xyz)">1</button><button type="button" onclick="spoilWord(xyz)">SP</button><button type="button" onclick="quoteWord(xyz)">Q</button><button type="button" onclick="listWord(xyz)">LI</button><button type="button" onclick="linkWord(xyz, 1)">A</button><input id="inOne" value="http://"></div>'
-var cont2 =   '<div id="ButtonContainerRep"><button type="button" onclick="test(zyx)" style="">Preview</button><button type="button" onclick="strikeWord(zyx)"><strike>S</strike></button><button type="button" onclick="boldWord(zyx)"><b>B</b></button><button type="button" onclick="underlineWord(zyx)"><u>U</u></button><button type="button" onclick="italizeWord(zyx)"><i>I</i></button><button type="button" onclick="unorderWord(zyx)">*</button><button type="button" onclick="orderWord(zyx)">1</button><button type="button" onclick="spoilWord(zyx)">SP</button><button type="button" onclick="quoteWord(zyx)">Q</button><button type="button" onclick="listWord(zyx)">LI</button><button type="button" onclick="linkWord(zyx, 2)">A</button><input id="inTwo" value="http://"></div>'
+var cont1 =   '<div id="ButtonContainerNew"><button type="button" title="Preview" onclick="test(xyz)" style="">Preview</button><button type="button" title="Strikethrough" onclick="strikeWord(xyz)"><strike>S</strike></button><button type="button" title="Bold" onclick="boldWord(xyz)"><b>B</b></button><button type="button" title="Underline" onclick="underlineWord(xyz)"><u>U</u></button><button type="button" title="Italize" onclick="italizeWord(xyz)"><i>I</i></button><button type="button" title="Unordered list (sets style, the articles need to be wrapped in LI-elements)" onclick="unorderWord(xyz)">ul</button><button type="button" title="Ordered list (sets style, the articles need to be wrapped in LI-elements)" onclick="orderWord(xyz)">ol</button><button type="button" title="Spoiler (needs PonyDebate, not marked in preview)" onclick="spoilWord(xyz)">SP</button><button type="button" title="Blockquote" onclick="quoteWord(xyz)">Q</button><button type="button" title="Creates list-element (if not inside an ol-block unordered)" onclick="listWord(xyz)">LI</button><button type="button" title="Hyperlink around selection (Link drawn from input-field)" onclick="linkWord(xyz, 1)">A</button><input id="inOne" title="Link for the A-tag" value="http://"></div>'
+var cont2 =   '<div id="ButtonContainerRep"><button type="button" title="Preview" onclick="test(zyx)" style="">Preview</button><button type="button" title="Strikethrough" onclick="strikeWord(zyx)"><strike>S</strike></button><button type="button" onclick="boldWord(zyx)"><b>B</b></button><button type="button" title="Underline" onclick="underlineWord(zyx)"><u>U</u></button><button type="button" title="Italize" onclick="italizeWord(zyx)"><i>I</i></button><button type="button" title="Unordered list (sets style, the articles need to be wrapped in LI-elements)" onclick="unorderWord(zyx)">ul</button><button type="button" title="Ordered list (sets style, the articles need to be wrapped in LI-elements)" onclick="orderWord(zyx)">ol</button><button type="button" title="Spoiler (needs PonyDebate, not marked in preview)" onclick="spoilWord(zyx)">SP</button><button type="button" title="Blockquote" onclick="quoteWord(zyx)">Q</button><button type="button" title="Creates list-element (if not inside an ol-block unordered)" onclick="listWord(zyx)">LI</button><button type="button" title="Hyperlink around selection (Link drawn from input-field)" onclick="linkWord(zyx, 2)">A</button><input id="inTwo" title="Link for the A-tag" value="http://"></div>'
 $("#IDCommentNewThreadText").before(cont1)
 $("#txtComment").before(cont2)
 
@@ -84,3 +84,24 @@ $("#txtComment").before(cont2)
 var xxxxxx = '<div style="position: fixed; background: white; top: 50%; height: 33%; padding: 3px; margin: 3px; border: 7px double black; overflow: scroll; display: none" id="dialog" title="Preview"><button onclick="negTest()" style="">Hide</button><u><b>PREVIEW</b></u><br><br></div>'
 $('#idc-container').after(xxxxxx)
 $('#dialog')[0].style.width = $('#idc-container')[0].clientWidth+'px'
+
+//------------------------
+//CSS for buttons---------
+//------------------------
+    a=$('#ButtonContainerNew')[0].children
+    for (i=0;i<a.length-1;i++){
+      a[i].style.backgroundColor = "greenyellow"
+      a[i].style.margin = "0px 3px 2px 0px"
+      a[i].style.border = "1px solid black"
+    }  
+    a[11].style.margin = "0px 3px 2px 0px"
+    a[11].style.border = "1px solid black"
+
+    a=$('#ButtonContainerRep')[0].children
+    for (i=0;i<a.length-1;i++){
+      a[i].style.backgroundColor = "greenyellow"
+      a[i].style.margin = "0px 3px 2px 0px"
+      a[i].style.border = "1px solid black"
+    }  
+    a[11].style.margin = "0px 3px 2px 0px"
+    a[11].style.border = "1px solid black"
