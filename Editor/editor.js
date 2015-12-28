e5cba0a2e0db158201637d5bab0448d50df533e3
Piperita <Piperita@users.noreply.github.com>
2015-12-28 21:30:17 +0100

    Added <p> and integrated filter-function

--- Editor/editor.js
+++ Editor/editor.js
@@ -1,4 +1,3 @@
-
 //-------------------------------------
 //Area for button-functions------------
 //-------------------------------------
@@ -41,12 +40,41 @@ function listWord(ver){
   ver.surroundSelectedText("<li>","</li>")
 }
 
-function orderWord(ver){
-  ver.surroundSelectedText("<ol>","</ol>")
+function parWord(ver){
+  ver.surroundSelectedText("<p>","</p>")
 }
 
+function filterWord(ver, box){
+  content = ver[0].value;
+  content = content.replace('cialis','cia<b></b>lis');
+  content = content.replace('patreon','pat<b></b>reon');
+  content = content.replace('carrier','car<b></b>rier');
+  content = content.replace('viagra','via<b></b>gra');
+  content = content.replace('penis','pen<b></b>is');
+  content = content.replace('cock','co<b></b>ck'); 
+  content = content.replace('pharmacy','phar<b></b>macy');
+  content = content.replace('nigger','nig<b></b>ger');
+  ver[0].value = content;
+  if (box == 1){
+    postComment(0);
+  } else if (box == 2){
+    postComment(1);
+  }
+}
+
+
 function unorderWord(ver){
-  ver.surroundSelectedText("<ul>","</ul>")
+ ver.surroundSelectedText("<ul>","</ul>")
+ r=ver.getSelection();
+ r=r.text.replace(/#/g, "<li>")
+ ver.replaceSelectedText(r);
+}
+
+function orderWord(ver){
+ ver.surroundSelectedText("<ol>","</ol>")
+ r=ver.getSelection();
+ r=r.text.replace(/#/g, "<li>")
+ ver.replaceSelectedText(r);
 }
 
 function linkWord(ver, link){
@@ -73,8 +101,8 @@ var zyx = $("#txtComment")
 //-----------------------
 //Creates Button-Area----
 //-----------------------
-var cont1 =   '<div id="ButtonContainerNew"><button type="button" title="Preview" onclick="test(xyz)" style="">Preview</button><button type="button" title="Strikethrough" onclick="strikeWord(xyz)"><strike>S</strike></button><button type="button" title="Bold" onclick="boldWord(xyz)"><b>B</b></button><button type="button" title="Underline" onclick="underlineWord(xyz)"><u>U</u></button><button type="button" title="Italize" onclick="italizeWord(xyz)"><i>I</i></button><button type="button" title="Unordered list (sets style, the articles need to be wrapped in LI-elements)" onclick="unorderWord(xyz)">ul</button><button type="button" title="Ordered list (sets style, the articles need to be wrapped in LI-elements)" onclick="orderWord(xyz)">ol</button><button type="button" title="Spoiler (needs PonyDebate, not marked in preview)" onclick="spoilWord(xyz)">SP</button><button type="button" title="Blockquote" onclick="quoteWord(xyz)">Q</button><button type="button" title="Creates list-element (if not inside an ol-block unordered)" onclick="listWord(xyz)">LI</button><button type="button" title="Hyperlink around selection (Link drawn from input-field)" onclick="linkWord(xyz, 1)">A</button><input id="inOne" title="Link for the A-tag" value="http://"></div>'
-var cont2 =   '<div id="ButtonContainerRep"><button type="button" title="Preview" onclick="test(zyx)" style="">Preview</button><button type="button" title="Strikethrough" onclick="strikeWord(zyx)"><strike>S</strike></button><button type="button" onclick="boldWord(zyx)"><b>B</b></button><button type="button" title="Underline" onclick="underlineWord(zyx)"><u>U</u></button><button type="button" title="Italize" onclick="italizeWord(zyx)"><i>I</i></button><button type="button" title="Unordered list (sets style, the articles need to be wrapped in LI-elements)" onclick="unorderWord(zyx)">ul</button><button type="button" title="Ordered list (sets style, the articles need to be wrapped in LI-elements)" onclick="orderWord(zyx)">ol</button><button type="button" title="Spoiler (needs PonyDebate, not marked in preview)" onclick="spoilWord(zyx)">SP</button><button type="button" title="Blockquote" onclick="quoteWord(zyx)">Q</button><button type="button" title="Creates list-element (if not inside an ol-block unordered)" onclick="listWord(zyx)">LI</button><button type="button" title="Hyperlink around selection (Link drawn from input-field)" onclick="linkWord(zyx, 2)">A</button><input id="inTwo" title="Link for the A-tag" value="http://"></div>'
+var cont1 =   '<div id="ButtonContainerNew"><button type="button" title="Preview" onclick="test(xyz)" style="">Preview</button><button type="button" title="Strikethrough" onclick="strikeWord(xyz)"><strike>S</strike></button><button type="button" title="Bold" onclick="boldWord(xyz)"><b>B</b></button><button type="button" title="Underline" onclick="underlineWord(xyz)"><u>U</u></button><button type="button" title="Italize" onclick="italizeWord(xyz)"><i>I</i></button><button type="button" title="Unordered list (# starts list elements)" onclick="unorderWord(xyz)">ul</button><button type="button" title="Ordered list (# starts list elements)" onclick="orderWord(xyz)">ol</button><button type="button" title="Spoiler (needs PonyDebate, not marked in preview)" onclick="spoilWord(xyz)">SP</button><button type="button" title="Blockquote" onclick="quoteWord(xyz)">Q</button><button type="button" title="Creates list-element (needs a ol- or ul-wrapping)" onclick="listWord(xyz)">LI</button><button type="button" title="Creates paragraph (useful for RSS-reading; sets blank line above and under element)" onclick="parWord(xyz)">P</button><button type="button" title="Hyperlink around selection (Link drawn from input-field)" onclick="linkWord(xyz, 1)">A</button><input id="inOne" title="Link for the A-tag" value="http://"></div>'
+var cont2 =   '<div id="ButtonContainerRep"><button type="button" title="Preview" onclick="test(zyx)" style="">Preview</button><button type="button" title="Strikethrough" onclick="strikeWord(zyx)"><strike>S</strike></button><button type="button" title="Bold" onclick="boldWord(zyx)"><b>B</b></button><button type="button" title="Underline" onclick="underlineWord(zyx)"><u>U</u></button><button type="button" title="Italize" onclick="italizeWord(zyx)"><i>I</i></button><button type="button" title="Unordered list (# starts list elements)" onclick="unorderWord(zyx)">ul</button><button type="button" title="Ordered list (# starts list elements)" onclick="orderWord(zyx)">ol</button><button type="button" title="Spoiler (needs PonyDebate, not marked in preview)" onclick="spoilWord(zyx)">SP</button><button type="button" title="Blockquote" onclick="quoteWord(zyx)">Q</button><button type="button" title="Creates list-element (needs a ol- or ul-wrapping)" onclick="listWord(zyx)">LI</button><button type="button" title="Creates paragraph (useful for RSS-reading; sets blank line above and under element)" onclick="parWord(zyx)">P</button><button type="button" title="Hyperlink around selection (Link drawn from input-field)" onclick="linkWord(zyx, 2)">A</button><input id="inTwo" title="Link for the A-tag" value="http://"></div>'
 $("#IDCommentNewThreadText").before(cont1)
 $("#txtComment").before(cont2)
 
@@ -95,8 +123,8 @@ var eee = setTimeout(function(){
       a[i].style.margin = "0px 3px 2px 0px"
       a[i].style.border = "1px solid black"
     }  
-    a[11].style.margin = "0px 3px 2px 0px"
-    a[11].style.border = "1px solid black"
+    a[12].style.margin = "0px 3px 2px 0px"
+    a[12].style.border = "1px solid black"
 
     a=$('#ButtonContainerRep')[0].children
     for (i=0;i<a.length-1;i++){
@@ -104,6 +132,26 @@ var eee = setTimeout(function(){
       a[i].style.margin = "0px 3px 2px 0px"
       a[i].style.border = "1px solid black"
     }  
-    a[11].style.margin = "0px 3px 2px 0px"
-    a[11].style.border = "1px solid black"
+    a[12].style.margin = "0px 3px 2px 0px"
+    a[12].style.border = "1px solid black"
+ 
+    $('.idc-btn_l')[2].href="javascript:filterWord(xyz,1);";
+    $('.idc-btn_l')[3].href="javascript:filterWord(zyx,2);";
+    
+    id_add_action('page_load', function(pageObj) { 
+      $('.idc-btn_l')[2].href="javascript:filterWord(xyz,1);";
+      $('.idc-btn_l')[3].href="javascript:filterWord(zyx,2);";
+    });
+    
+    id_add_action('thread_page_load', function(pageObj) {
+      $('.idc-btn_l')[2].href="javascript:filterWord(xyz,1);";
+      $('.idc-btn_l')[3].href="javascript:filterWord(zyx,2);";
+    });
+    
+    id_add_action('comment_post', function(commentObj) {
+      $('.idc-btn_l')[2].href="javascript:filterWord(xyz,1);";
+      $('.idc-btn_l')[3].href="javascript:filterWord(zyx,2);";
+    });
+    
+    
 },333)
